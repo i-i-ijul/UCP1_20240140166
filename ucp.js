7bf0157447771e7e6b3c52d43de8471db4f735aa
i-i-ijul <haqqikamvret@gmail.com>
2026-04-23 09:39:50 +0700

    first commit

---
+++ ucp.js
@@ -0,0 +1,17 @@
+const video = document.querySelector("video");
+const audio = document.querySelector("audio");
+const images = document.querySelector("picture img");
+
+
+video.addEventListener("click", function images(){
+    alert("ijul's setup");
+});
+
+audio.addEventListener("play", function audio(){
+    alert("Scott Street - Phoebe Bridgers");
+});
+
+images.addEventListener("click", function images(){
+    alert("Adalaj gwej");
+});
+
